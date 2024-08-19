e614b9273af8eae621ae181fd9e80bc31acafb25
rasmusjerlov <rasmus.jerloev@gmail.com>
2024-08-19 09:17:38 +0200

    asd

--- L01/Person.cs
+++ L01/Person.cs
@@ -1,6 +1,35 @@
-namespace L01;
-
-public class Person
+namespace L01
 {
+    internal class Person
+    {
+        public Person(string fornavn)
+        {
+            this.fornavn = fornavn;
+        }
+
+        private string fornavn
+        {
+            get
+            {
+                return Fornavn;
+            }
+            set
+            {
+                Fornavn = value;
+            }
+        }
+        public string Fornavn
+        {
+            get
+            {
+                return fornavn;
+            }
+            set
+            {
+                fornavn = value;
+            }
+        }
+    }
     
 }
+
