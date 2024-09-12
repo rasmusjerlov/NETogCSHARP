2d3fdcfe122a54e9c11722f23bd3e66a4e367859
rasmusjerlov <rasmus.jerloev@gmail.com>
2024-09-12 09:09:49 +0200

    asd

--- WebApplication1/Controllers/CalculatorController.cs
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -5,13 +5,26 @@ namespace WebApplication1.Controllers;
 public class CalculatorController : Controller
 {
     // GET
-    public IActionResult Index()
+    
+    public IActionResult TimeCalculatorView()
     {
         return View();
     }
-    
-    public IActionResult TimeCalculator()
+
+    public IActionResult CalculateTime(IFormCollection formcollection)
     {
-        return View();
+        int hours = Convert.ToInt32(formcollection["Hours"]);
+        int minutes = Convert.ToInt32(formcollection["Minutes"]);
+        int seconds = Convert.ToInt32(formcollection["Seconds"]);
+
+        TimeSpan ts = new TimeSpan(0, hours, minutes, seconds);
+        double total = ts.TotalSeconds;
+
+        ViewBag.hours = hours;
+        ViewBag.minutes = minutes;
+        ViewBag.seconds = seconds;
+        ViewBag.totalInSeconds = total;
+        
+        return View("TimeCalculatorResult");
     }
 }
